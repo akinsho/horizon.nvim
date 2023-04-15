6a30bf97b9bcf40c2bbaa70176be88815d62fd75
Akin Sowemimo <22454918+akinsho@users.noreply.github.com>
2023-04-15 11:42:09 +0100

    style(formatting): add dangling commas

--- lua/horizon/palette-light.lua
+++ lua/horizon/palette-light.lua
@@ -2,18 +2,18 @@ local colors = {
   active_line_number_fg = '#817B7C',
   bg = '#FDF0ED',
   class_variable = {
-    fg = '#DA103F'
+    fg = '#DA103F',
   },
   code_block = {
-    fg = '#DC3318'
+    fg = '#DC3318',
   },
   codelens_fg = '#FBDFD8',
   comment = {
     fg = '#989190',
-    italic = true
+    italic = true,
   },
   constant = {
-    fg = '#DC3318'
+    fg = '#DC3318',
   },
   cursor_bg = '#E84A72',
   cursor_fg = '#FDF0ED',
@@ -23,10 +23,10 @@ local colors = {
   error = '#F43E5C',
   fg = '#06060C',
   field = {
-    fg = '#DA103F'
+    fg = '#DA103F',
   },
   func = {
-    fg = '#1D8991'
+    fg = '#1D8991',
   },
   git_added_fg = '#60C9A0',
   git_deleted_fg = '#F43E5C',
@@ -38,24 +38,24 @@ local colors = {
   indent_guide_fg = '#FBDDD5',
   keyword = {
     fg = '#8A31B9',
-    italic = true
+    italic = true,
   },
   link = {
-    fg = '#F6661E'
+    fg = '#F6661E',
   },
   match_paren = '#FBDFD8',
   operator = {
-    fg = '#333333'
+    fg = '#333333',
   },
   parameter = {
-    italic = true
+    italic = true,
   },
   pmenu_bg = '#FADAD1',
   pmenu_item_sel_fg = '#E84A72',
   pmenu_thumb_bg = '#FBDFD8',
   pmenu_thumb_fg = '#F9D1C7',
   regex = {
-    fg = '#DC3318'
+    fg = '#DC3318',
   },
   sidebar_bg = '#FDF0ED',
   sidebar_fg = '#504C4F',
@@ -63,40 +63,40 @@ local colors = {
   sign_deleted_bg = '#F67387',
   sign_modified_bg = '#60C1C0',
   special_keyword = {
-    fg = '#8A31B9'
+    fg = '#8A31B9',
   },
   statusline_active_fg = '#F9CBBE',
   statusline_bg = '#FDF0ED',
   statusline_fg = '#504C4F',
   storage = {
     fg = '#8A31B9',
-    italic = true
+    italic = true,
   },
   string = {
-    fg = '#F6661E'
+    fg = '#F6661E',
   },
   structure = {
-    fg = '#F77D26'
+    fg = '#F77D26',
   },
   tag = {
     fg = '#DA103F',
-    italic = true
+    italic = true,
   },
   term_cursor_bg = '#06060C',
   term_cursor_fg = '#FAD8CF',
   title = {
-    fg = '#DA103F'
+    fg = '#DA103F',
   },
   type = {
-    fg = '#F77D26'
+    fg = '#F77D26',
   },
   variable = {
-    fg = '#DA103F'
+    fg = '#DA103F',
   },
   visual = '#FBDFD8',
   warning = '#60C9A0',
   winbar = '#FADAD1',
-  winseparator_fg = '#E6DAD8'
+  winseparator_fg = '#E6DAD8',
 }
 
 return colors
